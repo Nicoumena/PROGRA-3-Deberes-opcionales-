1936cd642987d94bfeaa681e9015e593a2588a32
Nicoumena <damian.mena@udla.edu.ec>
2025-05-21 16:31:34 -0500

    Add files via upload

---
+++ ventanafinal.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+
+public class ventanafinal {
+    private JPanel panel1;
+    private JTabbedPane tabbedPane1;
+    private JPanel main;
+    private JSpinner spID;
+    private JTextField txtNombreRobot;
+    private JComboBox cboNivelEnergia;
+    private JTextField txtPuntaje;
+    private JComboBox cboCategoria;
+    private JTextArea txtListaRobots;
+    private JButton btnAgregar;
+    private JButton btnBuscar;
+    private JTextArea txtCategoriaEncontrada;
+    private JTextField txtID;
+    private JButton btnEditar;
+    private JTextArea txtMostrarRobotEditado;
+    private JButton btnOrdenar;
+}
